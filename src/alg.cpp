e73fc66eb12ed3273d7ed930cc112f1e8d5597b6
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-04-09 21:57:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,32 +4,32 @@
 #define INCLUDE_TSTACK_H_
 template<typename T, int size>
 class TStack {
-  private:
-    T arr[100];
-    int m;
-  public:
-    TStack(): m(-1) {}
-    void push(T value) {
-      if (!isFull())
-        arr[++m] = value;
-      else
-        throw std::string("Full");
-    }
-    T get() const {
-      return arr[m];
-    }
-    T pop() {
-      if (isEmpty())
-        throw std::string("Empty");
-      else
-        return arr[m--];
-    }
-    bool isEmpty() const {
-      return m == -1;
-    }
-    bool isFull() const {
-      return m == size - 1;
-    }
+private:
+  T arr[100];
+  int m;
+public:
+  TStack(): m(-1) {}
+  void push(T value) {
+    if (!isFull())
+      arr[++m] = value;
+    else
+      throw std::string("Full");
+  }
+  T get() const {
+    return arr[m];
+  }
+  T pop() {
+    if (isEmpty())
+      throw std::string("Empty");
+    else
+      return arr[m--];
+  }
+  bool isEmpty() const {
+    return m == -1;
+  }
+  bool isFull() const {
+    return m == size - 1;
+  }
 };
 #endif
 #include <map>
