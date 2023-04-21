e267d5e84b0ba6f4b5a21ccbd574ea44379355a2
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-04-21 15:04:43 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,39 +5,39 @@
 template<typename T, int size>
 class TStack {
 private:
-   T arr[100];
-   int m;
+    T arr[100];
+    int m;
 
 public:
-   TStack() : m(-1) {}
-   void push(T value) {
-       if (!isFull())
-           arr[++m] = value;
-       else
-           throw std::string("Full");
-   }
-   T get() const {
-       return arr[m];
-   }
-   T pop() {
-       if (isEmpty())
-           throw std::string("Empty");
-       else
-           return arr[m--];
-   }
-   bool isEmpty() const {
-       return m == -1;
-   }
-   bool isFull() const {
-       return m == size - 1;
-   }
-   T top() {
-       if (!isEmpty()) {
-           return arr[m];
-       }
-       else {
-           exit(EXIT_FAILURE);
-       }
-   }
+    TStack() : m(-1) {}
+    void push(T value) {
+        if (!isFull())
+            arr[++m] = value;
+        else
+            throw std::string("Full");
+    }
+    T get() const {
+        return arr[m];
+    }
+    T pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[m--];
+    }
+    bool isEmpty() const {
+        return m == -1;
+    }
+    bool isFull() const {
+        return m == size - 1;
+    }
+    T top() {
+        if (!isEmpty()) {
+            return arr[m];
+        }
+        else {
+            exit(EXIT_FAILURE);
+        }
+    }
 };
 #endif
